65df70d2a7bc3e78f4cbbd9343a788b4c2b67b65
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-02 19:01:57 -0400

    Complete bachelor 2

--- class13-materials/bachelor-code/2/js/main.js
+++ class13-materials/bachelor-code/2/js/main.js
@@ -1,25 +1,25 @@
-const andi = document.querySelector('#andi')
-const claire = document.querySelector('#claire')
-const sharleen = document.querySelector('#sharleen')
+const andi = document.querySelector("#andi");
+const claire = document.querySelector("#claire");
+const sharleen = document.querySelector("#sharleen");
 
-document.querySelector('#andiNext').addEventListener('click', /*INSERTCODE*/)
-document.querySelector('#claireNext').addEventListener('click', /*INSERTCODE*/)
-document.querySelector('#sharleenNext').addEventListener('click', /*INSERTCODE*/)
+document.querySelector("#andiNext").addEventListener("click", andiNext);
+document.querySelector("#claireNext").addEventListener("click", claireNext);
+document.querySelector("#sharleenNext").addEventListener("click", sharleenNext);
 
-function andiNext(){
-	claire.classList.add(/*INSERTCODE*/)
-	sharleen.classList.add(/*INSERTCODE*/)
-	andi.classList.toggle(/*INSERTCODE*/)
+function andiNext() {
+  claire.classList.add("hidden");
+  sharleen.classList.add("hidden");
+  andi.classList.toggle("hidden");
 }
 
-function claireNext(){
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
+function claireNext() {
+  sharleen.classList.add("hidden");
+  andi.classList.add("hidden");
+  claire.classList.toggle("hidden");
 }
 
-function sharleenNext(){
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
+function sharleenNext() {
+  andi.classList.add("hidden");
+  claire.classList.add("hidden");
+  sharleen.classList.toggle("hidden");
 }
