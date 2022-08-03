3103b7d9fe643d5d9afb0ce45e1c70105fa29fa6
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-03 13:30:57 -0400

    Redo js and add another value button

--- not-simple-calculator/js/main.js
+++ not-simple-calculator/js/main.js
@@ -1,40 +1,39 @@
-let total = 0;
+// declare total variable with starting value of 0
+let total = 0; //using let since it will be reassigned multiple times
 
-document.querySelector("#pumpkin").addEventListener("click", makeZero);
-document.querySelector("#dominosPizza").addEventListener("click", jumanji);
-document.querySelector("#zebra").addEventListener("click", add9);
-document.querySelector("#cantThinkOfAnything").addEventListener("click", sub2);
-document.querySelector("#utOh").addEventListener("click", add16);
+// get all elements when smurf clicks
+document.querySelector("#reset").addEventListener("click", makeZero);
+document.querySelector("#apple").addEventListener("click", add3);
+document.querySelector("#strawberry").addEventListener("click", add9);
+document.querySelector("#mango").addEventListener("click", sub2);
+document.querySelector("#kiwi").addEventListener("click", add16);
 
+// make zero and show in result
 function makeZero() {
   total = 0;
-  document.querySelector("#placeToPutResult").innerText = total;
+  document.querySelector("#result").innerText = total;
 }
 
-function jumanji() {
-  total = total + 3;
-  document.querySelector("#placeToPutResult").innerText = total;
-}
-
-function add9() {
-  total = total + 9;
-  document.querySelector("#placeToPutResult").innerHTML = total;
-}
-
-function sub2() {
-  total = total - 2;
-  document.querySelector("#placeToPutResult").innerHTML = total;
-}
-
-function add16() {
-  total = total + 16;
-  document.querySelector("#placeToPutResult").innerHTML = total;
-}
-
-// declare total variable with starting value of 0
-// get all elements when smurf clicks
-// make zero and show in result
 // add3 to total and show in result
+function add3() {
+  total += 3;
+  document.querySelector("#result").innerText = total;
+}
+
 // add 9 to total and show in result
+function add9() {
+  total += 9;
+  document.querySelector("#result").innerText = total;
+}
+
 // subtract 2 to total and show in result
-// add another value button
+function sub2() {
+  total -= 2;
+  document.querySelector("#result").innerText = total;
+}
+
+// add another value button - add 16 to total and show in result
+function add16() {
+  total += 16;
+  document.querySelector("#result").innerText = total;
+}
