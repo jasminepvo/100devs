9b4e2a55cd90ac5eb137be26538290d79ff35037
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-04 16:53:18 -0400

    Create 2 basic functions

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,20 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1, num2) {
+  const sum = num1 + num2;
+  alert(sum);
+}
+addTwoNumbers(4, 5);
 
 //create a function that multiplys three numbers and console logs the product
 
+function multiplysThreeNumbers(num1, num2, num3) {
+  const product = num1 * num2 * num3;
+  console.log(product);
+}
+multiplysThreeNumbers(4, 5, 2);
+
 //create a function that divides two numbers and returns the ???
+function dividesTwoNums(n1, n2) {
+  return n1 / n2;
+}
+console.log(dividesTwoNums(4, 2));
