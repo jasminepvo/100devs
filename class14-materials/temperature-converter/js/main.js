bf2f8c56f992e4349a104eb2ac66c17b6295f953
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-04 16:17:39 -0400

    Create temp converter

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+//only on click
+document.querySelector("#convert").addEventListener("click", check);
+
+function check() {
+  //get value out of input
+  let temp = document.querySelector("#celsius").value;
+  //convert the value
+  temp = temp * (9 / 5) + 32;
+  //show value to user
+  document.querySelector("#result").innerText = temp;
+}
