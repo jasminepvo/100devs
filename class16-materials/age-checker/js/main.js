e566bd875f2cf0eaacbfca064b4450d6ea553ffb
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-10 16:09:06 -0400

    Create function to check age and display results in the console

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -5,9 +5,39 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+/*
+let age = 30;
 
+if (age < 16) console.log("You can not drive");
+else if (age < 18)
+  console.log(
+    "Can't hate from outside the club, because you can't even get in"
+  );
+else if (age < 21) console.log("Can't drink");
+else if (age < 25) console.log("Can't rent cars afforadbly");
+else if (age < 30) console.log("Can't rent fancy cars affordably");
+else console.log("There is nothing left to look forward too");
+*/
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector("h1").addEventListener("click", print);
+
+function print() {
+  printResult = result();
+  document.querySelector("p").innerText = printResult;
+}
+
+function result() {
+  const age = document.querySelector("#danceDanceRevolution").value;
+  if (age < 16) return "You can not drive";
+  else if (age < 18)
+    return "Can't hate from outside the club, because you can't even get in";
+  else if (age < 21) return "Can't drink";
+  else if (age < 25) return "Can't rent cars afforadbly";
+  else if (age < 30) return "Can't rent fancy cars affordably";
+  else return "There is nothing left to look forward too";
+}
