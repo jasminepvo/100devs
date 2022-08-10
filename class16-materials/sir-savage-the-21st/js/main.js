fe5702358e4c9a2be98dbda9e83b797924ea641d
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-10 16:57:34 -0400

    Create loop to print 21, 21 times for 21 savage

--- class16-materials/sir-savage-the-21st/js/main.js
+++ class16-materials/sir-savage-the-21st/js/main.js
@@ -1,2 +1,11 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+
+function savage21() {
+  for (i = 0; i < 21; i++) {
+    document.querySelector("#savageSays").innerText += " 21";
+  }
+}
+savage21();
+
+//go back later make it a button you click and have voice
