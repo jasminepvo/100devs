fe9035a29200f643cda7311985686a1b32afbad6
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-13 19:11:01 -0400

    Create array, store element of array in variable, get length of array and get last element of the array

--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,16 @@
 //Create an array of movies with at least three movies.
+let movies = [
+	"bring it on",
+	"bring it on again",
+	"bring it on: in it to win it",
+];
 
 //Using the array from above, store the first movie in a variable
+let firstMovie = movies[0];
 
 //Get the length of the original array and store it in a new variable
+let movieLength = movies.length();
+console.log(movieLength);
 
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastMovie = movies[movieLength - 1]; //-1 bc index starts at 0
