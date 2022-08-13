c4eaf9618b21c8a50143226f9d7d95a003f93e53
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-13 19:31:32 -0400

    Loop through array, add to array and replace element, find average of all elements in array

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,25 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
+//Create an array of movie titles. Loop through the array and add each element to the h2.
+let movies = ["Bring it on", "Bring it on again", "Fight to the finish"];
+for (let i = 0; i < movies.length; i++) {
+	document.querySelector("h2").innerText += movies[i];
+}
 
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
+let arrOfNums = [10, 20, 30];
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
+arrOfNums.forEach((item, i) => {
+	arrOfNums[i] = item + 3;
+});
 
+//Find the average of all the numbers from question two
+// let sum = 0;
+// arrOfNums.forEach((num) => {
+//   sum += num;
+// })
 
-//Find the average of all the numbers from question three
+let sum = 0;
+
+for (let i = 0; i < arrOfNums.length; i++) {
+	sum += arrOfNums[i];
+}
+console.log(sum / arrOfNums.length); //60 / 3 = 20
