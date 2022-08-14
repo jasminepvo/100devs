5affbeddb14fd995d21823a6dbc41f08703d23e0
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 16:09:25 -0400

    For loop that prints to the dom

--- super-review/sir-savage-the-21st/js/main.js
+++ super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,9 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+
+function savage() {
+	for (let i = 1; i <= 21; i++) {
+		document.querySelector("#savageSays").innerText += " 21";
+	}
+}
+savage();
