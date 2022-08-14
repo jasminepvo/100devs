03614a513861e22c2e80ea496bc2c211973101d0
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 14:17:52 -0400

    Return a new array containing even number from original array

--- class20-materials /review-winter-games/js/main.js
+++ class20-materials /review-winter-games/js/main.js
@@ -1,1 +1,35 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+//find evens
+// //fat arrow ES6
+// const evens = (numbers) => {
+// 	for (const num of numbers) {
+// 		if (num % 2 === 0) {
+// 			console.log(num);
+// 		}
+// 	}
+// };
+// evens([1, 2, 3, 4]);
+
+// //function declaration
+// function findEvens(numbers) {
+// 	for (const num of numbers) {
+// 		if (num % 2 === 0) {
+// 			console.log(num);
+// 		}
+// 	}
+// }
+// findEvens([1, 2, 3, 4]);
+
+//Leon's solution
+function onlyEvens(nums) {
+	let evens = [];
+
+	nums.forEach((n) => {
+		if (n % 2 === 0) {
+			evens.push(n);
+		}
+	});
+	return evens;
+}
+console.log(onlyEvens([1, 2, 3, 4, 5, 6]));
