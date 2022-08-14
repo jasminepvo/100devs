1beaa17ea7506389fead8b69c68ac69f7a4e0236
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 14:01:23 -0400

    Loop through array and multiply each element together, alert the product

--- class20-materials /review-summer-games/js/main.js
+++ class20-materials /review-summer-games/js/main.js
@@ -1,1 +1,15 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
+// const multiply = (arr) => {
+
+// }
+
+// for (const arr of myArray) {
+// 	console.log(arr);
+// }
+
+function multiNumsInArr(arr) {
+	let product = 1;
+	arr.forEach((num) => (product *= num));
+	alert(product);
+}
+multiNumsInArr([2, 3, 4]);
