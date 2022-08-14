13b36f295a874e587a8f4245397fb308cdbaf5d0
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 13:38:33 -0400

    Check index of first and last element in array, compare and alert appropriate message

--- class20-materials /review-in-paradise-after-paradise/js/main.js
+++ class20-materials /review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,13 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+let nums = [10, 20, 30, 40, 50];
+
+function checkFirstAndLast(nums) {
+	if (nums[0] < nums[nums.length - 1]) {
+		alert("Hi");
+	} else if (nums[0] > nums[nums.length - 1]) {
+		alert("Bye");
+	} else {
+		alert("We close in an hour");
+	}
+}
