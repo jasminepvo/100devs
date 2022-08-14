cfecba9519d2b8a9c91ce969f3b966eb9eb67f6b
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 15:41:28 -0400

    Conditionals if / else if

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -5,7 +5,23 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+let age = 16;
 
+if (age < 16) {
+	console.log("can't drive");
+} else if (age < 18) {
+	console.log("can't hate from outside");
+} else if (age < 21) {
+	console.log("can't drink");
+} else if (age < 25) {
+	console.log("can't rent cars");
+} else if (age < 30) {
+	console.log("can't rent fancy cars");
+} else if (age < 30 || age > 30) {
+	console.log("nothing to look forward to");
+} else {
+	console.log("something cool");
+}
 
 //--- Harder
 //On click of the h1
