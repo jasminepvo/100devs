eb4a6184561a33e63e00ceb9033a7a9509910742
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 15:57:38 -0400

    Temp converter using document.querySelector and function

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,12 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+//convert from celsius to fahrenheit
+//get celsius value
+//conversion - store in fahrenheit
+
+document.querySelector("h1").addEventListener("click", convert);
+
+function convert() {
+	let usrInput = Number(document.querySelector("input").value);
+	let fahrenheit = usrInput * (9 / 5) + 32;
+	document.querySelector("h2").innerText = fahrenheit;
+}
