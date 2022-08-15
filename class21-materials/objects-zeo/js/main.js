c29a9c12954eb579d6d4569a2310a5ac6c38d5a4
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-15 18:54:23 -0400

    Create object with four properties and three methods

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,26 @@
 //Create a stopwatch object that has four properties and three methods
+// const stopwatch = {
+//   hour: "04",
+//   minute: "21",
+//   second: "59",
+//   currentTime function() {
+//     return `${this.hour}${this.minute}${this.second}`
+//   },
+// }
+
+let stopwatch = {};
+
+stopwatch.brand = "Nike";
+stopwatch.color = "black";
+stopwatch.shape = "round";
+stopwatch.size = "medium";
+
+stopwatch.start = function () {
+	console.log("startttinggggg");
+};
+stopwatch.stop = function () {
+	console.log("stopppppinnggg");
+};
+stopwatch.sayBrand = function () {
+	console.log(stopwatch.brand);
+};
