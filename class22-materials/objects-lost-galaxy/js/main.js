6b67eac0050522ddd59f9e14580c0e2eca4ba67d
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-16 16:21:37 -0400

    Practice making objects

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,53 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {};
+
+mouse.color = "white";
+mouse.brand = "off brand";
+mouse.model = "unknown";
+mouse.wireless = true;
+
+mouse.leftClick = function () {
+	console.log("LEFT CLICKKK");
+};
+mouse.rightClick = function () {
+	console.log("LEFT CLICKKK");
+};
+mouse.scroll = function () {
+	console.log("SCOLLLLL");
+};
+
+let mouse2 = {};
+
+mouse2.color = "silver";
+mouse2.brand = "Apple";
+mouse2.model = "Magic Trackpad";
+mouse2.wireless = true;
+
+mouse2.leftClick = function () {
+	console.log("LEFT CLICKKK");
+};
+mouse2.rightClick = function () {
+	console.log("LEFT CLICKKK");
+};
+mouse2.scroll = function () {
+	console.log("SCOLLLLL");
+};
+
+let mouse3 = {};
+
+mouse3.color = "white";
+mouse3.brand = "Logictech";
+mouse3.model = "MX9";
+mouse3.wireless = true;
+
+mouse3.leftClick = function () {
+	console.log("LEFT CLICKKK");
+};
+mouse3.rightClick = function () {
+	console.log("LEFT CLICKKK");
+};
+mouse3.scroll = function () {
+	console.log("SCOLLLLL");
+};
+
+//constructor lets you to set something up once then pass the values in
