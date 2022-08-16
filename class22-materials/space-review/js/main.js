8aa2950cab0d6ee3e4739ddb3876ae0277d583c2
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-16 15:52:54 -0400

    reduce() method

--- class22-materials/space-review/js/main.js
+++ class22-materials/space-review/js/main.js
@@ -1,6 +1,11 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+nums = [1, 2, 3, 4, 5];
+
+let sum = nums.reduce((acc, c) => acc + c, 0); //acc is accumulator, c is current value, 0 starts at 0
+// reduce will run once for each element in array. grabs 2 values (accumulator and current valuelet sum = nums.reduce((acc, c) => +acc +c); // codewars answer w javascript tricks
+alert(sum);
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
