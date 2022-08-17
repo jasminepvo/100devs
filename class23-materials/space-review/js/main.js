52c1a7f975854e87705c49d6280196da88d38514
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-16 21:45:35 -0400

    Arrays

--- class23-materials/space-review/js/main.js
+++ class23-materials/space-review/js/main.js
@@ -1,9 +1,17 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
-
+let nums = [1, 2, 3, 4];
+let sum = nums.reduce((acc, c) => acc + c, 0);
+alert(sum);
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+const numbers = (numbers) => {
+	for (const num of nums) {
+		num = num ** 2;
+	}
+};
+numbers([1, 2, 3, 4]);
 
 //Create a function that takes string
 //Print the reverse of that string to the console
