841c9a4965408d8c998310feb761c34d04c5060b
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 12:32:37 -0400

    reverse string and palindrome checker

--- class23-materials/space-review/js/main.js
+++ class23-materials/space-review/js/main.js
@@ -3,18 +3,24 @@
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 let nums = [1, 2, 3, 4];
 let sum = nums.reduce((acc, c) => acc + c, 0);
-alert(sum);
+//alert(sum);
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-const numbers = (numbers) => {
-	for (const num of nums) {
-		num = num ** 2;
-	}
-};
-numbers([1, 2, 3, 4]);
+let newArr = (nums) => nums.map((num) => Math.pow(num, 2));
+console.log(nums);
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+let unoReverse = (str) => console.log(str.split("").reverse().join(""));
+//split turns it into an array
+//reverse reverses the array
+//join turns array back into string
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+const palindromeCheck = (str) =>
+	alert(str === str.split("").reverse().join(""));
+
+palindromeCheck("petep");
