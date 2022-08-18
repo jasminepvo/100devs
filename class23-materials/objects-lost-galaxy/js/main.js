9b5947ce85185278d25abb3052419a36a92f7ebc
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 12:48:57 -0400

    Create object

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a dog object that has four properties and three methods
+
+const dog = {};
+
+dog.name = "danger";
+dog.breed = "pitbull husky";
+dog.color = "brown";
+dog.owner = "Jasmine";
+dog.bark = function () {
+	console.log("WOOF");
+};
+dog.jump = function () {
+	console.log("BLAST OFF");
+};
+dog.fetch = function () {
+	console.log("ZOOOOOOOMMM");
+};
