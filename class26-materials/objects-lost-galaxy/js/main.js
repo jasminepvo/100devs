2a32308ebc1bb1d34661b9d7598e22f71db5b176
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 15:53:02 -0400

    Create object

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,18 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+function Pizza(crust, sauce, topping, cheese) {
+	this.crust = crust;
+	this.sauce = sauce;
+	this.topping = topping;
+	this.cheese = cheese;
+	this.status = function () {
+		console.log("I am on the way");
+	};
+	this.type = function () {
+		return `I am made with ${topping}`;
+	};
+	this.brand = function () {
+		console.log("Papa Johns");
+	};
+}
+
+//oops i made this a constructor
