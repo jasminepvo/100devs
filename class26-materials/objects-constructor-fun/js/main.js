34cbdbc8abaea793181e295128c528f14bbb4fff
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 18:16:59 -0400

    Create constructor

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,16 @@
 //Create a constructor with 4 properties and 3 methods
+function Pizza(crust, sauce, topping, cheese) {
+	this.crust = crust;
+	this.sauce = sauce;
+	this.topping = topping;
+	this.cheese = cheese;
+	this.status = function () {
+		console.log("I am on the way");
+	};
+	this.type = function () {
+		return `I am made with ${topping}`;
+	};
+	this.brand = function () {
+		console.log("Papa Johns");
+	};
+}
