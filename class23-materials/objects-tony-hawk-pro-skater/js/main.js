0e1ac86f9f3f2b5d9f9ac6f3dcbe7658b869e13a
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 12:58:19 -0400

    Create constructor

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,23 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function TonyHawkCharacter(chName, chStance, chSponsor, chMove) {
+	this.characterName = chName;
+	this.stance = chStance;
+	this.sponsor = chSponsor;
+	this.specialMove = chMove;
+	this.flip = function () {
+		console.log("kickflip");
+	};
+	this.taunt = function () {
+		console.log(`dont make me use my ${this.specialMove}`);
+	};
+	this.grab = function () {
+		console.log("melon grabbb");
+	};
+}
+
+let kareemCampbell = new TonyHawkCharacter(
+	"Kareem",
+	"Goofy",
+	"Element",
+	"Ghetto Bird"
+);
