96ebd0289442c6a82ab80c7b1c6dfb6412d4274a
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 20:24:21 -0400

    Quotes API

--- class26-materials/api1/js/main.js
+++ class26-materials/api1/js/main.js
@@ -1,16 +1,28 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+	// fetch("https://api.goprogram.ai/inspiration/")
+	// 	.then((res) => res.json()) // parse response as JSON
+	// 	.then((data) => {
+	// 		console.log(data);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(`error ${err}`);
+	// 	});
+	var url = "https://api.goprogram.ai/inspiration";
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url);
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+			document.querySelector("h2").innerText = xhr.responseText;
+		}
+	};
+
+	console.log(typeof xhr.responseText);
+	xhr.send();
 }
