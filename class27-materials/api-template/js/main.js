930529865027ab62a3b70269bac2841d3e1d86de
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-19 18:43:25 -0400

    Get data from nasa using API & custom token

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -3,12 +3,14 @@ document.querySelector("button").addEventListener("click", getFetch);
 
 function getFetch() {
 	const choice = document.querySelector("input").value.toLowerCase();
-	const url = "https://pokeapi.co/api/v2/pokemon/" + choice;
+	const url = `https://api.nasa.gov/planetary/apod?api_key=tZ9fp8kS2gSxcFivT0DB8lBaEuEXPrsnjAS376eE&date=${choice}`;
 
 	fetch(url)
 		.then((res) => res.json()) // parse response as JSON
 		.then((data) => {
 			console.log(data);
+			document.querySelector("img").src = data.hdurl;
+			document.querySelector("h3").innerText = data.explanation;
 		})
 		.catch((err) => {
 			console.log(`error ${err}`);
