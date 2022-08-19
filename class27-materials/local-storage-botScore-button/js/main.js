b436c31d7c163ba5e30b9e2c68ba0116a9aaa629
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-19 19:24:47 -0400

    Add to local storage

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,13 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
 
+if (!localStorage.setItem("botScore")) {
+	localStorage.setItem("botScore", 0);
+}
 
+document.querySelector("button").addEventListener("click", addAnothaOne);
+
+function addAnothaOne() {
+	let botScoreVal = Number(localStorage.getItem("botScore"));
+	botScoreVal += 1;
+	localStorage.setItem("botScore", botScoreVal);
+}
