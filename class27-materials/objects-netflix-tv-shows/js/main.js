4a82d4ea5f44384ae5abca58f99134be06d89479
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-19 12:07:27 -0400

    Create class with constructor and methods

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,25 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+class NetflixTvShows {
+	constructor(genre, title, releaseDate, rating) {
+		this.title = title;
+		this.genre = genre;
+		this.date = releaseDate;
+		this.rating = rating;
+	}
+	play() {
+		consolelog("Playing...");
+	}
+	stop() {
+		consolelog("Stopping...");
+	}
+	saveToList() {
+		consolelog("Saved To List");
+	}
+}
 
-
+let bridgerton = new NetflixTvShows(
+	"Bridgerton",
+	"Period Romantic Drama",
+	2020,
+	"99%"
+);
