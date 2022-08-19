aebdf0f38d316b9776304bb04d092ac80aaab1e5
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-19 19:10:59 -0400

    Add videos from API

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -9,7 +9,11 @@ function getFetch() {
 		.then((res) => res.json()) // parse response as JSON
 		.then((data) => {
 			console.log(data);
-			document.querySelector("img").src = data.hdurl;
+			if (data.media_type === "image") {
+				document.querySelector("img").src = data.hdurl;
+			} else if (data.media_type === "video") {
+				document.querySelector("iframe").src = data.url;
+			}
 			document.querySelector("h3").innerText = data.explanation;
 		})
 		.catch((err) => {
