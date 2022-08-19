350f6da0d3de4cbd71fec0e552c28d9174efa1ac
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-19 14:48:11 -0400

    Pokemon template

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -1,17 +1,16 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+	const choice = document.querySelector("input").value.toLowerCase();
+	const url = "https://pokeapi.co/api/v2/pokemon/" + choice;
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
 }
-
