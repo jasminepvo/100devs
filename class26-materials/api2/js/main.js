fdb2da136462adab2f192e68d077012b4fadfb24
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-19 18:15:46 -0400

    Attempt the quoting machine api

--- class26-materials/api2/js/main.js
+++ class26-materials/api2/js/main.js
@@ -1,16 +1,16 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+	const url = "https://nodejs-quoteapp.herokuapp.com/quote";
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((response) => {
+			console.log(response);
+			document.querySelector("h2").innerText = response.quote;
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
 }
