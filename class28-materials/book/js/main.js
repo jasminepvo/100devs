58cd55091d626ab98a658ea5e9a8351405ceb217
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-23 19:55:24 -0400

    Add data to local storage and display on DOM

---
+++ class28-materials/book/js/main.js
@@ -0,0 +1,29 @@
+//Example fetch using pokemonapi.co
+document.querySelector("button").addEventListener("click", getFetch);
+//have to put outside the function so it stores on page load
+document.querySelector("h3").innerText = localStorage.getItem("books");
+
+function getFetch() {
+	const choice = document.querySelector("input").value;
+	console.log(choice);
+	const url = `https://openlibrary.org/isbn/${choice}.json`;
+
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
+}
+
+// if (!localStorage.getItem("books")) {
+//   localStorage.setItem("books", data.title);
+// } else {
+//   // get every book already in localStorage then add new title
+//   let books = localStorage.getItem("books") + " " + data.title;
+//   //put new title into localStorage to update
+//   localStorage.setItem("books", books);
+// }
+// document.querySelector("h3").innerText = localStorage.getItem("books");
