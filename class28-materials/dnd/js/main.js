dccb77c792e5857e918be6408e74c4ef851e7be4
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-23 17:44:02 -0400

    Fetch array of data while creating a new element and appending it

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -1,17 +1,26 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+	const choice = document.querySelector("input").value;
+	const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((data) => {
+			console.log(data.subclasses);
+			//can't hardcode bc it's an array
+			data.subclasses.forEach((obj) => {
+				//create an li
+				const li = document.createElement("li");
+				//add text to li
+				li.textContent = obj.name;
+				//append the li to the ul
+				document.querySelector("ul").appendChild(li);
+			});
+			//document.querySelector("ul").innerText = data.subclasses[0].name;
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
 }
-
