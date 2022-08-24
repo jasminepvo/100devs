d11e58f104745f5f0dd2086ab684f801c8e94bd0
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-23 20:00:29 -0400

    Redo code

--- class28-materials/book/js/main.js
+++ class28-materials/book/js/main.js
@@ -12,12 +12,23 @@ function getFetch() {
 		.then((res) => res.json()) // parse response as JSON
 		.then((data) => {
 			console.log(data);
+			//if nothing in books\
+			if (!localStorage.getItem("books")) {
+				localStorage.setItem("books", data.title);
+			} else {
+				//get every book in local storage and add new title
+				let books = localStorage.getItem("books") + " ; " + data.title;
+				//put new title into local storage to update
+				localStorage.setItem("books", books);
+			}
+			document.querySelector("h3").innerText = localStorage.getItem("books");
 		})
 		.catch((err) => {
 			console.log(`error ${err}`);
 		});
 }
 
+//REDOOOO
 // if (!localStorage.getItem("books")) {
 //   localStorage.setItem("books", data.title);
 // } else {
