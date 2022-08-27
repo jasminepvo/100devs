854e26869a7cbc15a633efa7bde76d475ae01a7c
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-27 14:33:28 -0400

    Make a class with a constructor with 4 properties and 3 methods

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,23 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class Espresso {
+	constructor(coffee, sugar, creamer, size) {
+		this.coffee = coffee;
+		this.sugar = sugar;
+		this.creamer = creamer;
+		this.size = size;
+	}
+
+	makeCoffee() {
+		console.log("making coffee...");
+	}
+	getSize() {
+		this.size = prompt("how many oz of coffee would you like?");
+	}
+	coffeeInfo() {
+		console.log(
+			`Your ${this.coffee} has ${this.sugar}, ${this.creamer} and is ${this.size} oz.`
+		);
+	}
+}
+
+let vanillaLate = new Espresso("vanilla", "none", "hazelnut", "8");
