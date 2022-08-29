025b778be93722af7fa23b71399a83cf9278ad39
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-29 14:18:27 -0400

    Use extend and super

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,31 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
+class Animal {
+	constructor(name) {
+		this.name = name;
+	}
+	speak() {
+		console.log(`${this.name} makes a sound`);
+	}
+}
 
+class Dog extends Animal {
+	constructor(name, breed) {
+		super(name);
+		this.breed = breed;
+	}
+	about() {
+		console.log(`${name} is a ${this.breed}`);
+	}
+}
+
+let jax = new Dog("jax", "mini goldendoodle");
+
+class Cat extends Animal {
+	constructor(name, breed) {
+		super(name);
+		this.breed = breed;
+	}
+	speak() {
+		console.log("meoowwww");
+	}
+}
